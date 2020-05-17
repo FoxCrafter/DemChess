98ec5dcc7187806a3b19d1258d3d73a57b80609c
Gbour Fox <07fuga@gmail.com>
2020-05-17 22:41:39 +0200

    srand

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "ui.h"
 
+#include <ctime>
+#include <cstdlib>
+
 int main(int argc, char const *const *argv) {
+    std::srand(std::time(0));
     Game game;
     ui::UI screen;
     screen.main_loop();
